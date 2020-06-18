1f3fd438397d604912734f1bf108a89f8087ebae
Livenow14 <xowjd41@naver.com>
2020-06-18 19:38:31 +0900

    1장_ 첫 번째 JUnit 테스트 만들기

---
+++ Chapter01/src/test/java/ScoreCollectionTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ScoreCollectionTest {
+
+    @Test
+    public void test(){
+        //given
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(()->5);
+        collection.add(()->7);
+
+        //when
+        int actualResult = collection.arithmeticMean();
+
+        //then
+        assertThat(actualResult).isEqualTo(6);
+    }
+
+}
